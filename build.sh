e265522702082cbdc35a7392562d0f8f5e1c3dae
Fahad Murtaza <fahdim@gmail.com>
2017-05-03 04:21:41 +0500

    Updated docker specific integration, added details to readme file, some code cleanup and updated the app to setup ports in a docker friendly way

---
+++ build.sh
@@ -0,0 +1,17 @@
+echo "Creatong the docker container"
+
+docker build --rm -t fahdi/palettro .
+
+echo "Done building the docker image!"
+
+echo "Setting up the docker container"
+
+docker run -d -p 5984:8000 fahdi/palettro
+
+echo "Congrats, your docker app is running!"
+
+printf "\n\n##############################\n\n"
+
+printf "Browse: http://localhost:5984"
+
+printf "\n\n##############################\n\n"
